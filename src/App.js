15a3db62f2b124c960e0e67342168a9f2dda2c98
Gokulcasper <ronitcasper215@gmail.com>
2022-08-07 19:51:21 +0530

    Feat : form-validation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [Name, setName] = useState([]);
+  const [Submit, setSubmit] = useState();
+
+  const handelChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handelClick = (e) => {
+    e.preventDefault();
+    setSubmit(Name);
+  };
+
+  return (
+    <div className="App">
+      <h1>user {Name}</h1>
+      <h2> username {Submit}</h2>
+      <form onSubmit={handelClick}>
+        <input
+          type="text"
+          placeholder="Enter your Name"
+          onChange={handelChange}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
